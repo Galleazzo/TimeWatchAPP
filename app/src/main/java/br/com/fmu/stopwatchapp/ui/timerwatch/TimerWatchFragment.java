07ba32c298137e6f5e5363a4765b56b37873a12e
Galleazzo <paulogalleazzo1@gmail.com>
2023-11-22 22:05:02 -0300

    bug fix in timer

--- app/src/main/java/br/com/fmu/stopwatchapp/ui/timerwatch/TimerWatchFragment.java
+++ app/src/main/java/br/com/fmu/stopwatchapp/ui/timerwatch/TimerWatchFragment.java
@@ -1,8 +1,10 @@
 package br.com.fmu.stopwatchapp.ui.timerwatch;
 
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,15 +24,15 @@ public class TimerWatchFragment extends Fragment {
 
     private Button start;
     private Button configTime;
-    private Button finish;
+    private Button finishCount;
     private Button pause;
     private TextView textView;
 
     private CountDownTimer temporizador;
     private boolean isTimerRunning = false;
-    private long timeLeftInMillis = 0; // Tempo inicial
-
+    private long timeLeftInMillis = 0;
     private FragmentTimerwatchBinding binding;
+    private Vibrator vibrator;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -41,34 +43,35 @@ public class TimerWatchFragment extends Fragment {
         View root = binding.getRoot();
 
 
-        textView = binding.textTimerWatch;
-        start = binding.buttonStart;
-        configTime = binding.buttonConfigTime;
-        finish = binding.buttonFinish;
-        pause = binding.buttonPause;
+        this.textView = binding.textTimerWatch;
+        this.start = (Button) binding.buttonStart;
+        this.configTime = (Button) binding.buttonConfigTime;
+        this.finishCount = (Button) binding.buttonFinish;
+        this.pause = (Button) binding.buttonPause;
 
-        configTime.setOnClickListener(new View.OnClickListener() {
+        this.vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        this.configTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showTimePickerDialog();
             }
         });
 
-        start.setOnClickListener(new View.OnClickListener() {
+        this.start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startTimer();
             }
         });
 
-        pause.setOnClickListener(new View.OnClickListener() {
+        this.pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 pauseTimer();
             }
         });
 
-        this.finish.setOnClickListener(new View.OnClickListener() {
+        this.finishCount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 resetTimer();
@@ -76,14 +79,19 @@ public class TimerWatchFragment extends Fragment {
         });
 
         updateCountDownText();
-
         final TextView textView = binding.textTimerWatch;
 
+        this.start.setEnabled(false);
+        this.pause.setEnabled(false);
+        this.finishCount.setEnabled(false);
 
         return root;
     }
 
     private void showTimePickerDialog() {
+        if (this.vibrator != null) {
+            this.vibrator.vibrate(50);
+        }
         int selectedMinutes = (int) (timeLeftInMillis / 1000) / 60;
 
         TimePickerDialog timePickerDialog = new TimePickerDialog(
@@ -108,34 +116,58 @@ public class TimerWatchFragment extends Fragment {
     }
 
     private void startTimer() {
+        if (this.vibrator != null) {
+            this.vibrator.vibrate(50);
+        }
         if (!isTimerRunning) {
             temporizador = new CountDownTimer(timeLeftInMillis, 1000) {
                 @Override
                 public void onFinish() {
                     updateCountDownFinish();
                 }
+
                 @Override
                 public void onTick(long millisUntilFinished) {
                     timeLeftInMillis = millisUntilFinished;
                     updateCountDownText();
-
                 }
-            };
+            }.start();
+            this.configTime.setEnabled(false);
+            this.start.setEnabled(false);
+            this.pause.setEnabled(true);
+            this.finishCount.setEnabled(true);
             isTimerRunning = true;
         }
     }
 
     private void pauseTimer() {
+        if (this.vibrator != null) {
+            this.vibrator.vibrate(50);
+        }
         if (isTimerRunning) {
+            this.start.setEnabled(true);
+            this.pause.setEnabled(false);
             temporizador.cancel();
             isTimerRunning = false;
         }
     }
 
     private void resetTimer() {
-        timeLeftInMillis = 0; // Reinicia o tempo para 0
-        updateCountDownFinish();
+        if (this.vibrator != null) {
+            this.vibrator.vibrate(50);
+        }
+        if (temporizador != null) {
+            temporizador.cancel();
+            isTimerRunning = false;
+        }
+        this.finishCount.setEnabled(false);
+        this.start.setEnabled(false);
+        this.pause.setEnabled(false);
+        this.configTime.setEnabled(true);
+        timeLeftInMillis = 0;
+        updateCountDownText();
     }
+
     private void updateCountDownFinish(){
         String timeLeftFormatted = "00:00:00";
         textView.setText(timeLeftFormatted);
@@ -149,6 +181,19 @@ public class TimerWatchFragment extends Fragment {
 
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
         textView.setText(timeLeftFormatted);
+
+        if (buttonEnabled())
+            this.start.setEnabled(true);
+    }
+
+    private Boolean buttonEnabled() {
+        if ((this.textView.getText().toString() != "00:00:00" && this.timeLeftInMillis != 0) && (!this.pause.isEnabled() && !this.finishCount.isEnabled())){
+            return true;
+        }
+        if ((this.textView.getText().toString() != "00:00:00" && this.timeLeftInMillis != 0) && (this.pause.isEnabled() && this.finishCount.isEnabled())){
+            return false;
+        }
+        return false;
     }
 
     @Override
